0dcc4324de0580c0cda48ff7575d853025fd45a8
Angelos Bouklis <me@arjix.dev>
2025-11-03 02:50:03 +0200

    Update build script to initialize and populate keys

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 IFS=$'\n\t'
 
 sudo pacman-key --init
+sudo pacman-key --populate archlinux
+paru -Sy archlinux-keyring --noconfirm
+
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
