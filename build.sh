d654e393264d918bc229b9b1ffb1bdad292b39ad
Angelos Bouklis <me@arjix.dev>
2025-11-11 09:39:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-paru -Sy archlinux-keyring --noconfirm
+paru -Sy archlinux-keyring chaotic-keyring --noconfirm
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
